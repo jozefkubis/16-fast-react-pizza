db7058332abd86b79d690122530c7dac326cdedb
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-09-16 21:58:55 +0200

    Pokracujuce prace na tailwind css

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Jozef</div>;
+  return <div className="hidden text-sm font-semibold md:block">Jozef</div>;
 }
 
 export default Username;
