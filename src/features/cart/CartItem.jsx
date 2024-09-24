0c9e1df973aa284ad7fc059ee2a6b362a87f0f43
jozef kubis <kubis.jozef@outlook.com>
2024-09-24 23:17:49 +0200

    Deleting Cart Items

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,5 +1,5 @@
-import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import DeleteItem from "./DeleteItem";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
@@ -9,9 +9,13 @@ function CartItem({ item }) {
       <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div className="flex items-center justify-between sm:gap-6">
-        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+
+      <div className="flex items-center gap-10">
+        <div className="flex items-center justify-between sm:gap-6">
+          <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        </div>
+
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
