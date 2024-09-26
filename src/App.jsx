d9b974d9961ca21f443763aabd836374c4600f61
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-09-26 21:08:38 +0200

    Updating Data Without Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Home from "./ui/Home"
-import Menu, { loader as menuLoader } from "./features/menu/Menu"
-import Cart from "./features/cart/Cart"
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
 import CreateOrder, {
   action as createOrderAction,
-} from "./features/order/CreateOrder"
-import Order, { loader as orderLoader } from "./features/order/Order"
-import AppLayout from "./ui/AppLayout"
-import Error from "./ui/Error"
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
 
 const router = createBrowserRouter([
   {
@@ -37,13 +38,14 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
-])
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
