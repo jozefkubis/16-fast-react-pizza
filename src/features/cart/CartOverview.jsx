61f2450c5d9c84d4010a24d252683fa21dc712ba
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-09-30 20:01:48 +0200

    uprava formatCurrency v CartOverview

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuantity);
@@ -12,7 +13,7 @@ function CartOverview() {
     <div className="itemx-center flex justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:p-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
-        <span>${totalCartPrice}</span>
+        <span>${formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
